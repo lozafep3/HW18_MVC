e6c0d550916178c7843281a6cd1475e1603aa170
Arthur Loza <loza.uns@gmail.com>
2017-08-12 03:15:58 +0300

    HW18_MVC: lodash templates added

--- public/js/components/main.js
+++ public/js/components/main.js
@@ -15,28 +15,37 @@ function getUsers () {
 }
 
 function renderDatabaseError () {
-    let databaseErrorCode = `<div class="user">No connection to database</div>`;
+    let template = _.template($('#database-error-template').html());
+    let data = {
+        error: 'No connection to database'
+    };
 
-    $(databaseErrorCode).appendTo('.userlist-wrapper');
+    $(template(data)).appendTo('.userlist-wrapper');
     $('.user').addClass('database-error');
 }
 
 function renderUsers (data) {
     $.each(data, function(key, value) {
-        let userCode = `<form class="user"><span class="db-id">${value.id}</span> <span class="db-name">${value.name}</span> <span class="db-email">${value.email}</span><input type="button" class="button delete" value="Delete"><input type="button" class="button edit" value="Edit"></form>`;
+        let template = _.template($('#user-view-template').html());
+        let data = {
+            id: value.id,
+            name: value.name,
+            email: value.email
+        };
 
-        $(userCode).appendTo('.userlist-wrapper');
+        $(template(data)).appendTo('.userlist-wrapper');
     });
 }
 
 function editUser () {
-    let currentCode = $(this).parent().html();
-    let id = $(this).parent().find('.db-id').text();
-    let name = $(this).parent().find('.db-name').text();
-    let email = $(this).parent().find('.db-email').text();
-    let userEditCode = `<input class="db-id" value="${id}"> <input class="db-name" value="${name}"> <input class="db-email" value="${email}"> <input type="button" class="button back" value="Back"><input type="button" class="button save" value="Save">`;
+    let template = _.template($('#user-edit-template').html());
+    let data = {
+        id: $(this).parent().find('.db-id').text(),
+        name: $(this).parent().find('.db-name').text(),
+        email: $(this).parent().find('.db-email').text()
+    };
 
-    $(this).parent().data('code', currentCode).html(userEditCode);
+    $(this).parent().html(template(data));
 }
 
 function deleteUser () {
@@ -44,12 +53,14 @@ function deleteUser () {
 }
 
 function saveUser () {
-    let id = $(this).parent().find('.db-id').val();
-    let name = $(this).parent().find('.db-name').val();
-    let email = $(this).parent().find('.db-email').val();
-    let editedCode = `<span class="db-id">${id}</span> <span class="db-name">${name}</span> <span class="db-email">${email}</span><input type="button" class="button delete" value="Delete"><input type="button" class="button edit" value="Edit">`;
+    let template = _.template($('#user-edited-template').html());
+    let data = {
+        id: $(this).parent().find('.db-id').val(),
+        name: $(this).parent().find('.db-name').val(),
+        email: $(this).parent().find('.db-email').val()
+    };
 
-    $(this).parent().html(editedCode);
+    $(this).parent().html(template(data));
 }
 
 function backUser () {
